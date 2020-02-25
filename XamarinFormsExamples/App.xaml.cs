d48c049d39ba7604c61cdd4bff2bf05e475aabb7
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-02-25 11:12:55 +0300

    MVVM
    MVVM ile EntryCell Kullanımı.

--- XamarinFormsExamples/App.xaml.cs
+++ XamarinFormsExamples/App.xaml.cs
@@ -12,7 +12,8 @@ namespace XamarinFormsExamples
 
             //MainPage = new UserView() { BindingContext = new UserViewModel()};
             //MainPage = new ListViewTextCell() { BindingContext = new DepartmentViewModel() };
-            MainPage = new ListViewImageCell() { BindingContext = new DepartmentViewModel() };
+            //MainPage = new ListViewImageCell() { BindingContext = new DepartmentViewModel() };
+            MainPage = new ListViewEntryCell() { BindingContext = new DepartmentViewModel() };
         }
 
         protected override void OnStart()
