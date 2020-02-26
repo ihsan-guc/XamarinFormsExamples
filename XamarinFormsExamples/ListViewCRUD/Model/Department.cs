44efb6aec45be1834f666df7f8cc2b5b85324ef9
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-02-26 18:23:48 +0300

    .

--- XamarinFormsExamples/ListViewCRUD/Model/Department.cs
+++ XamarinFormsExamples/ListViewCRUD/Model/Department.cs
@@ -3,17 +3,9 @@ using System.Runtime.CompilerServices;
 
 namespace XamarinFormsExamples.ListViewCRUD.Model
 {
-    public class Department : INotifyPropertyChanged
+    public class Department
     {
-        public int departmentId;
-        public int DepartmentId { get { return departmentId; } set { departmentId = value; OnPropertyChanged(); } }
-        public string departmentName { get; set; }
-        public string DepartmentName { get { return departmentName; } set { departmentName = value; OnPropertyChanged(); } }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        void OnPropertyChanged([CallerMemberName] string property = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
-        }
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
     }
 }
