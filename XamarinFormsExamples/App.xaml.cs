b66e147ef729989b2c50dc2727d453c3abcbdcfa
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-02-26 17:08:59 +0300

    MVVM
    MVVM ile ViewCell Kullanımı

--- XamarinFormsExamples/App.xaml.cs
+++ XamarinFormsExamples/App.xaml.cs
@@ -14,7 +14,8 @@ namespace XamarinFormsExamples
             //MainPage = new ListViewTextCell() { BindingContext = new DepartmentViewModel() };
             //MainPage = new ListViewImageCell() { BindingContext = new DepartmentViewModel() };
             //MainPage = new ListViewEntryCell() { BindingContext = new DepartmentViewModel() };
-            MainPage = new ListViewSwitchCell() { BindingContext = new DepartmentViewModel() };
+            //MainPage = new ListViewSwitchCell() { BindingContext = new DepartmentViewModel() };
+            MainPage = new ListViewViewCell() { BindingContext = new DepartmentViewModel() };
         }
 
         protected override void OnStart()
--- XamarinFormsExamples/ListView/ViewModel/DepartmentViewModel.cs
+++ XamarinFormsExamples/ListView/ViewModel/DepartmentViewModel.cs
@@ -13,10 +13,10 @@ namespace XamarinFormsExamples.ListView.ViewModel
         {
             model = new List<Department>();
             model.Add(new Department() { DepartmentId = 1, DepartmentName="Department Of Computer", ImageSource="picture.png" ,IsCheck=false});
-            model.Add(new Department() { DepartmentId = 2, DepartmentName="Department Of Computer", ImageSource="picture.png" ,IsCheck=true });
-            model.Add(new Department() { DepartmentId = 3, DepartmentName="Department Of Computer", ImageSource="picture.png" ,IsCheck=false });
-            model.Add(new Department() { DepartmentId = 4, DepartmentName="Department Of Computer", ImageSource="picture.png" ,IsCheck=true });
-            model.Add(new Department() { DepartmentId = 5, DepartmentName="Department Of Computer", ImageSource="picture.png" ,IsCheck=false });
+            model.Add(new Department() { DepartmentId = 2, DepartmentName= "Staff Department", ImageSource="picture.png" ,IsCheck=true });
+            model.Add(new Department() { DepartmentId = 3, DepartmentName= "Accounting Department", ImageSource="picture.png" ,IsCheck=false });
+            model.Add(new Department() { DepartmentId = 4, DepartmentName= "Computing Department", ImageSource="picture.png" ,IsCheck=true });
+            model.Add(new Department() { DepartmentId = 5, DepartmentName= "Revir", ImageSource="picture.png" ,IsCheck=false });
         }
         private List<Department> model;
         public List<Department> Model
