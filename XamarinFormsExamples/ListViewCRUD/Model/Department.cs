44054ea07ead2ed5488d86b9a9c98415d2e75d4a
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-02-26 18:21:59 +0300

    MVVM
    MVVM ile ListView Create işlemleri.

--- XamarinFormsExamples/ListViewCRUD/Model/Department.cs
+++ XamarinFormsExamples/ListViewCRUD/Model/Department.cs
@@ -1,8 +1,19 @@
-﻿namespace XamarinFormsExamples.ListViewCRUD.Model
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace XamarinFormsExamples.ListViewCRUD.Model
 {
-    public class Department
+    public class Department : INotifyPropertyChanged
     {
-        public int DepartmentId { get; set; }
-        public string DepartmentName { get; set; }
+        public int departmentId;
+        public int DepartmentId { get { return departmentId; } set { departmentId = value; OnPropertyChanged(); } }
+        public string departmentName { get; set; }
+        public string DepartmentName { get { return departmentName; } set { departmentName = value; OnPropertyChanged(); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
     }
 }
--- XamarinFormsExamples/ListViewCRUD/ViewModel/DepartmentViewModels.cs
+++ XamarinFormsExamples/ListViewCRUD/ViewModel/DepartmentViewModels.cs
@@ -1,46 +1,68 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
 using XamarinFormsExamples.ListViewCRUD.Model;
 
 namespace XamarinFormsExamples.ListViewCRUD.ViewModel
 {
-	public class DepartmentViewModels : INotifyPropertyChanged
+    public class DepartmentViewModels : INotifyPropertyChanged
     {
-		public event PropertyChangedEventHandler PropertyChanged;
-		void OnPropertyChanged([CallerMemberName] string property = null)
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
-		}
-		private int id;
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+        public DepartmentViewModels()
+        {
+            addcommand = new Command(AddDepartment);
+        }
+        private string departmentname;
 
-		public int Id
-		{
-			get { return id; }
-			set { id = value; OnPropertyChanged(); }
-		}
-		private string departmentname;
-
-		public string DepartmentName
-		{
-			get { return departmentname; }
-			set { departmentname = value; OnPropertyChanged(); }
-		}
-		void DepartmentData()
-		{
-			departments = new List<Department>();
-			departments.Add(new Department() { DepartmentId = 1, DepartmentName = "Department Of Computer"});
-			departments.Add(new Department() { DepartmentId = 2, DepartmentName = "Staff Department"});
-			departments.Add(new Department() { DepartmentId = 3, DepartmentName = "Accounting Department"});
-			departments.Add(new Department() { DepartmentId = 4, DepartmentName = "Computing Department"});
-			departments.Add(new Department() { DepartmentId = 5, DepartmentName = "Revir" });
-		}
-		private List<Department> departments;
-		public List<Department> Departments
-		{
-			get { return departments; }
-			set { departments = value; OnPropertyChanged(); }
-		}
-
-	}
+        public string DepartmentName
+        {
+            get { return departmentname; }
+            set { departmentname = value; OnPropertyChanged(); }
+        }
+        void DepartmentData()
+        {
+            departments.Add(new Department() { DepartmentId = 1, DepartmentName = "Department Of Computer" });
+            departments.Add(new Department() { DepartmentId = 2, DepartmentName = "Staff Department" });
+            departments.Add(new Department() { DepartmentId = 3, DepartmentName = "Accounting Department" });
+            departments.Add(new Department() { DepartmentId = 4, DepartmentName = "Computing Department" });
+            departments.Add(new Department() { DepartmentId = 5, DepartmentName = "Revir" });
+        }
+        private ObservableCollection<Department> departments;
+        public ObservableCollection<Department> Departments
+        {
+            get
+            {
+                if (departments == null)
+                    departments = new ObservableCollection<Department>();
+                return departments;
+            }
+            set { departments = value; OnPropertyChanged(); }
+        }
+        public ICommand addcommand;
+        public ICommand AddCommand
+        {
+            get { return addcommand; }
+            set { addcommand = value; OnPropertyChanged(); }
+        }
+        public void AddDepartment()
+        {
+            var department = new Department()
+            {
+                DepartmentId = departments.Count + 1,
+                DepartmentName = DepartmentName
+            };
+            if (department != null) 
+            {
+                departments.Add(department);
+            }
+        }
+    }
 }
