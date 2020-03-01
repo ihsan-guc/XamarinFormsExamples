cc346eeee68e6b878cedeb905f0e31d2643a97a9
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-03-01 23:11:57 +0300

    Validasyon
    .

---
+++ XamarinFormsExamples/EmailValid/EmailEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinFormsExamples.EmailValid
+{
+    public class EmailEntry : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += Bindable_TextChanged;
+        }
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= Bindable_TextChanged;
+        }
+        private void Bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var email = e.NewTextValue;
+            var emailEntry = sender as Entry;
+            string validEmailPattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+            + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+            + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+            if (validEmailPattern.Contains(validEmailPattern))
+            {
+                emailEntry.BackgroundColor = Color.Red;
+            }
+            else
+            {
+                emailEntry.BackgroundColor = Color.Wheat;
+            }
+        }
+    }
+}
