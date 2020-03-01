c56470806b571ea184cd13692b1d82d3f8314e3b
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-03-01 22:51:55 +0300

    Validasyon
    Email Validasyon

---
+++ XamarinFormsExamples/EmailValid/EmailValidView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsExamples.EmailValid
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EmailValidView : ContentPage
+    {
+        public EmailValidView()
+        {
+            InitializeComponent();
+        }
+    }
+}
