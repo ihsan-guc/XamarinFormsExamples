6c3bcd30ce51428e50ca7feaca4a1d2da92862e6
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-03-02 19:33:01 +0300

    MVVM
    MVVM TabbedPage Kullanımı

---
+++ XamarinFormsExamples/TabbedPage/Model/Bank.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsExamples.TabbedPage.Model
+{
+    public class Bank
+    {
+        public string BankIban { get; set; }
+        public string BankCode { get; set; }
+        public People People { get; set; }
+    }
+}
---
+++ XamarinFormsExamples/TabbedPage/Model/People.cs
@@ -0,0 +1,15 @@
+﻿namespace XamarinFormsExamples.TabbedPage.Model
+{
+    public class People
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TCKN { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+        public Bank Bank { get; set; }
+    }
+}
