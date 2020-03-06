201bbc84012d0d0ba1eea5eb8eff77d47068a719
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-03-06 22:57:16 +0300

    SQLite
    SQLite Örnek.

---
+++ XamarinFormsExamples/SQLiteExample/IPeopleRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace XamarinFormsExamples.SQLiteExample
+{
+    public interface IPeopleRepository
+    {
+        Task<IEnumerable<People>> GetPersonelAsyncList();
+        Task<bool> AddPeople(People people);
+    }
+}
---
+++ XamarinFormsExamples/SQLiteExample/People.cs
@@ -0,0 +1,9 @@
+﻿namespace XamarinFormsExamples.SQLiteExample
+{
+    public class People
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
