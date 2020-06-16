69375ea47dde60c7de80724367ee7cf219392dc8
ihsan-guc <ihsanguc.33@gmail.com>
2020-06-16 18:55:47 +0300

    MVVM_Menu
    .

---
+++ XamarinFormsExamples/MVVM MENU/ViewModel/MenuViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinFormsExamples.MVVM_MENU.ViewModel
+{
+    public class MenuViewModel : INotifyPropertyChanged
+    {
+        private ICommand navigateCommand;
+        public ICommand NavigateCommand
+        {
+            get { return navigateCommand; }
+            set { navigateCommand = value; OnPropertyChanged(); }
+        }
+        public async void NavigateCommandClick()
+        {
+            NavigateCommand = new Command<Type>(async (Type pageType) =>
+            {
+                Page page = (Page)Activator.CreateInstance(pageType);
+                await App.Current.MainPage.Navigation.PushAsync(page);
+            });
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+    }
+}
